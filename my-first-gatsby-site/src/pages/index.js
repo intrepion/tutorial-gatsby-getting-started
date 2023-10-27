9e4d7502a5757e32ba8d67c3b22c830f05247e48
Oliver Forral <intrepion@gmail.com>
2023-10-26 19:30:24 -0600

    Task: Update the static image to use a photo from your local filesystem.

--- my-first-gatsby-site/src/pages/index.js
+++ my-first-gatsby-site/src/pages/index.js
@@ -7,8 +7,8 @@ const IndexPage = () => {
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
       <StaticImage
-        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
-        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+        alt="Black and White Maltese Puppy by Poodles 2 Doodles"
+        src="../images/pexels-poodles-doodles-1458916.jpg"
       />
     </Layout>
   )
