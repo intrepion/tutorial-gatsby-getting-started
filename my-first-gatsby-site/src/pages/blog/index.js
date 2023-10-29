d1ed52404d4cc621b6b1bfa1155b7ea963a12803
Oliver Forral <intrepion@gmail.com>
2023-10-29 02:29:00 -0600

    Update Blog page to link to each post.

--- my-first-gatsby-site/src/pages/blog/index.js
+++ my-first-gatsby-site/src/pages/blog/index.js
@@ -1,22 +1,23 @@
 import * as React from 'react'
-import { graphql } from 'gatsby'
+import { Link, graphql } from 'gatsby'
 import Layout from '../../components/layout'
 import Seo from '../../components/seo'
 
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
-      <ul>
       {
-        data.allMdx.nodes.map((node) => (
+        data.allMdx.nodes.map(node => (
           <article key={node.id}>
-            <h2>{node.frontmatter.title}</h2>
+            <h2>
+              <Link to={`/blog/${node.frontmatter.slug}`}>
+                {node.frontmatter.title}
+              </Link>
+            </h2>
             <p>Posted: {node.frontmatter.date}</p>
-            <p>{node.excerpt}</p>
           </article>
         ))
       }
-      </ul>
     </Layout>
   )
 }
@@ -28,6 +29,7 @@ export const query = graphql`
         frontmatter {
           date(formatString: "MMMM D, YYYY")
           title
+          slug
         }
         id
         excerpt
